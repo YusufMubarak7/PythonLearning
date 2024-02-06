c982f40175ea348fd30b436b317713a4b769f311
Yusuf Mubarak M <yusuf.mubarak.x.m.-nd@disney.com>
2024-02-06 23:27:33 +0530

    completed Number Datatypes and started operators

---
+++ Number_Datatypes_with_basic_programs.py
@@ -0,0 +1,35 @@
+'''
+Numeric Data Types in Python
+The numeric data type in Python represents the data that has a numeric value. A numeric value can be an integer, a floating point number,or even a complex number.
+These values are defined as Python int, Python float, and Python complex classes in Python.
+'''
+
+# Integers
+'''
+This value is represented by int class. It contains positive or negative whole numbers(without fractions or decimals). 
+In Python, there is no limit to how long an integer value can be.
+'''
+# Example
+Number = 10
+number = 100
+negative_no = -100
+
+# Float
+'''
+ This value is represented by the float class. It is a real number with a floating-point representation. It is specified by a decimal point. 
+ Optionally, the character e or E followed by a positive or negative integer may be appended to specify scientific notation.
+'''
+
+# Example
+Number = 10.0
+number = 100.0
+negative_no = -100.0
+
+# Complex
+'''
+A complex number is represented by a complex class.A number in the form of a ± bj
+where a is real part, bj is the imaginary part and the value of j is √ (- 1)(square root of minus 1)
+'''
+
+# Example
+complex_number = 10 + 5.0j
---
+++ Python_operators.py
@@ -0,0 +1,65 @@
+# Operators in Python
+'''
+
+1.Arithmetic Operators
+2.Comparison Operators
+3.Logical Operators
+4.Assignment Operators
+5.Identity Operators
+6.Membership Operators
+7.Bitwise Operators
+
+'''
+
+# Arithmetic Operators( Works for all single value datatypes or Numbers)
+'''
+Python Arithmetic Operators are used to perform mathematical operations like addition, subtraction, multiplication, and division.
+'''
+#1.Addition operator(+) = It is used to add two or more operands
+#Example
+number = 10
+Number = 10.0
+sum = number + Number
+print(sum)
+
+#2.Subtraction operator(-) = It is used to subract two or more operands
+#Example
+number = 10
+Number = 11
+sub = number - Number
+print(sub)
+
+#3.Multiplication operator(*) = It is used to two or more operands
+#Example
+number = 10
+Number = 11
+mul = number * Number
+print(mul)
+
+#3.Divison operator(/) = It is used to divide the first operand by the second
+#Example
+number = 10
+Number = 3
+div = number/Number
+print(div) # Output will be 3.3333333333333335 the output will be float
+
+#4.Floor Divison(//) = It is used to divide the first operand by the second
+#Example
+number = 10
+Number = 3
+div = number//Number
+print(div) # Output will be 3 as floor divison will give output as int
+
+#5.Modulus Operator(%) = It returns the remainder when the first operand is divided by the second
+#Example
+number = 129
+Number = 10
+rem = number % Number
+print(rem) # Output will be 9
+
+#6.Exponential operator(**) = In Python, ** is the exponentiation operator. It is used to raise the first operand to the power of the second.
+Number = 10
+pow = Number ** 2
+print(pow)
+
+
